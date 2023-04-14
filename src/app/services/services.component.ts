2dc261e043fb25fc4f179014e1bfaa80a83ea28c
besson <bessonloane@gmail.com>
2023-04-14 16:28:32 +0200

    navbar responsive et autres

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -10,20 +10,25 @@ export class ServicesComponent {
   services: any[] = [
     {
       img: 'assets/img/service1.png',
+      logo_img:'assets/img/servicePms.png',
       title: 'Full property management system',
       description: 'Fully customisable guest journeys, dynamic pricing, task management geared to fit your needs. Create your listings, insert pictures, description, rules and any relevant information.',
       buttonText: 'Discover our PMS'
     },
     {
       img: 'assets/img/service2.png',
+      logo_img:'assets/img/ServiceBranding.png',
       title: 'White label branding',
       description: 'Your own branded website fully integrated with a payment engine and channel manager presenting your full portfolio of properties.',
+      description_list:["Payment", "Payment","Booking request", "Back office"],
       buttonText: 'Discover our creations'
     },
     {
       img: 'assets/img/service3.png',
+      logo_img:'assets/img/ServiceSupport.png',
       title: 'Dedicated Support Team & Expert Advice',
       description: 'A specialised team available to support your journey from the beginning and throughout. Training and collaboration to allow you to work independently and together with the team.',
+      description_list:["Technical support","Training", "Professional advices"],
       buttonText: 'Discover our support'
     },
   ];
